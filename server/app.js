773310ebfc452867e1e1ad399e9daca2410387cf
OrdosX <i@ordosx.tech>
2020-06-23 17:51:39 +0800

    0.1.1 - 定义状态代码

--- server/app.js
+++ server/app.js
@@ -20,6 +20,12 @@ if(process.env.USE_HTTPS === "true") {
 const passphrase = sha256(process.env.PASSWORD);
 const tempDir = __dirname+'/temp'
 const downloadDir = __dirname+'/files';
+const CODE_SUCCESS = 0;
+const CODE_DOWNLOADING = 1;
+const CODE_CANCELING = 2;
+const CODE_DOWNLOAD_FAIL = -1;
+const CODE_NOT_FOUND = -2;
+const CODE_WRONG_INPUT = -3;
 
 // 接口：新建任务
 // 示例：{URL: "https://example.com/xxx", name: "xxx"}
@@ -33,38 +39,38 @@ app.post('/tasks', (req, res) => {
             var body = JSON.parse(jsonString);
         } catch (error) {
             console.log(jsonString);
-            res.json({ code: -3, errmsg: error.message});
+            res.json({ code: CODE_WRONG_INPUT, errmsg: error.message});
             return;
         }
         if(body.URL === '' || body.name == '' || body.URL === undefined || body.name == undefined) {
-            res.json({ code: -3, errmsg: 'URL与文件名不能为空'});
+            res.json({ code: CODE_WRONG_INPUT, errmsg: 'URL与文件名不能为空'});
             return;
         }
         var downloader = new DownloaderHelper(body.URL, tempDir, {fileName: body.name});
         var id = uuid();
-        tasks.set(id, {code: 1, progress: 0, errmsg: "", downloader: downloader});
+        tasks.set(id, {code: CODE_DOWNLOADING, progress: 0, errmsg: "", downloader: downloader});
         downloader.on("progress", (status) => {
             let t = tasks.get(id);
             t.progress = Number(status.progress).toFixed(1);
             tasks.set(id, t);
         })
         downloader.on("error", (err) => {
-            tasks.set(id, {code: -1, progress: 0, errmsg: err.message, downloader: null});
+            tasks.set(id, {code: CODE_DOWNLOAD_FAIL, progress: 0, errmsg: err.message, downloader: null});
             logger.fail(body.name, body.URL, err.message);
         })
         downloader.on("end", () => {
             let t = tasks.get(id);
-            if(t.code == 2) {
+            if(t.code == CODE_CANCELING) {
                 tasks.delete(req.params.id);
                 fs.unlinkSync(tempDir+'/'+body.name);
                 logger.fail(body.name, body.URL, 'Canceled.');
             } else {
                 fs.rename(tempDir+'/'+body.name, downloadDir+'/'+body.name, ()=>{});
-                tasks.set(id, {code: 0, progress: 100, errmsg: "", downloader: null});
+                tasks.set(id, {code: CODE_SUCCESS, progress: 100, errmsg: "", downloader: null});
                 logger.succ(body.name, body.URL);
             }
         })
-        res.json({code:0, ID:id});
+        res.json({code:CODE_SUCCESS, ID:id});
         downloader.start().catch(()=>{});
     })
 })
@@ -80,7 +86,7 @@ app.get('/tasks/:id', (req, res) => {
         }
         res.json({code: t.code, progress: t.progress, errmsg: t.errmsg});
     } else {
-        res.json({code: -2, progress: 0, errmsg: "任务不存在"});
+        res.json({code: CODE_NOT_FOUND, progress: 0, errmsg: "任务不存在"});
     }
 })
 
@@ -88,12 +94,12 @@ app.get('/tasks/:id', (req, res) => {
 app.delete('/tasks/:id', (req, res) => {
     if(tasks.has(req.params.id)) {
         let t = tasks.get(req.params.id);
-        t.code = 2;
+        t.code = CODE_CANCELING;
         tasks.set(req.params.id, t)
         t.downloader.stop().catch(()=>{});
-        res.json({code: 0, errmsg: ""});
+        res.json({code: CODE_SUCCESS, errmsg: ""});
     } else {
-        res.json({code: -2, errmsg: "任务不存在"});
+        res.json({code: CODE_NOT_FOUND, errmsg: "任务不存在"});
     }
 })
 
@@ -113,9 +119,9 @@ app.delete('/files/:name', (req, res) => {
     fs.exists(downloadDir+'/'+req.params.name, (exists) => {
         if(exists) {
             fs.unlinkSync(downloadDir+'/'+req.params.name);
-            res.json({ code: 0, errmsg: "" });
+            res.json({ code: CODE_SUCCESS, errmsg: "" });
         } else {
-            res.json({ code: -2, errmsg: "文件不存在"});
+            res.json({ code: CODE_NOT_FOUND, errmsg: "文件不存在"});
         }
     })
 })
