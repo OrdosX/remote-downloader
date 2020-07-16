36d667a6df7c36407c64d15faa8a102c9fb779f0
OrdosX <i@ordosx.tech>
2020-07-16 14:46:10 +0800

    优化文件体积

--- ui/src/main.js
+++ ui/src/main.js
@@ -1,11 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import { BootstrapVue, BIcon, BIconLink45deg, BIconX, BIconFileEarmarkText, BIconPlay, BIconStopFill, BIconShieldLock, BIconClipboard, BIconTrash } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 Vue.use(BootstrapVue)
-Vue.use(IconsPlugin)
+Vue.component('BIcon', BIcon)
+Vue.component('BIconX', BIconX)
+Vue.component('BIconPlay', BIconPlay)
+Vue.component('BIconTrash', BIconTrash)
+Vue.component('BIconStopFill', BIconStopFill)
+Vue.component('BIconLink45deg', BIconLink45deg)
+Vue.component('BIconClipboard', BIconClipboard)
+Vue.component('BIconShieldLock', BIconShieldLock)
+Vue.component('BIconFileEarmarkText', BIconFileEarmarkText)
 
 Vue.config.productionTip = false
 
