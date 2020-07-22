332beca170352dc1d58b5a1da71582a722d4b878
OrdosX <i@ordosx.tech>
2020-07-22 17:08:32 +0800

    优化详情显示

--- ui/src/components/FileList.vue
+++ ui/src/components/FileList.vue
@@ -8,7 +8,7 @@
       <b-tr v-for="f in files" :key="f.name">
         <b-td><span :id="f.name">{{ f.name }}</span></b-td>
         <b-tooltip :target="f.name">
-          <span>{{ f.modified }} | {{ f.size }} </span>
+          <span>{{ f.modified }}</span><br /><span>{{ f.size }}</span>
         </b-tooltip>
         <b-td class="text-right">
           <b-button variant="light" :href="f.URL" class="shadow-sm">
